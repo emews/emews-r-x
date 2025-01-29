1559a09913d6fc39956d40a42bf803eede8631fa
Justin Wozniak <woz@anl.gov>
2025-01-29 12:26:36 -0600

    Add svn for R

--- setup-conda.sh
+++ setup-conda.sh
@@ -10,8 +10,16 @@ echo
 
 conda env list
 
+PKGS=(
+  # Basic tools:
+  conda-build
+  anaconda-client
+  # Needed for R:
+  svn
+)
+
 set -x
-conda install --yes conda-build anaconda-client
+conda install --yes ${PKGS[@]}
 
 # Suppress this behavior from Conda on "conda build" errors:
 # Would you like conda to send this report to the core maintainers? [y/N]:
