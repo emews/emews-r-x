53ef27ffa60d5202d0dd94f05b41b8c73c4d35b4
Justin Wozniak <woz@anl.gov>
2025-02-28 15:16:17 -0600

    Report if FORCE is on

--- upload.sh
+++ upload.sh
@@ -22,15 +22,15 @@ PKG=$1
 
 if [[ ! -f $PKG ]] abort "upload.sh: Not found: PKG=$PKG"
 
-print "DIR:  ${PKG:h}"
-print "FILE: ${PKG:t}"
+print "DIR:   ${PKG:h}"
+print "FILE:  ${PKG:t}"
 
 zmodload zsh/mathfunc zsh/stat
 zstat -H A -F "%Y-%m-%d %H:%M:%S" $PKG
-print  "TIME: ${A[mtime]}"
-printf "SIZE: %.1f MB\n" $(( float(${A[size]}) / (1024*1024) ))
+print  "TIME:  ${A[mtime]}"
+printf "SIZE:  %.1f MB\n" $(( float(${A[size]}) / (1024*1024) ))
 HASH=( $( md5sum $PKG ) )
-print "HASH: ${HASH[1]}"
+print "HASH:  ${HASH[1]}"
 
 # Mac renice does not respect --version
 PRIORITY="--priority"
@@ -40,6 +40,7 @@ then
 fi
 renice $PRIORITY 19 $$ >& /dev/null
 
+if (( ${#FORCE} )) printf "FORCE: "
 printf "CONFIRM? "
 read -t 30 _ && print "YES" || print "TIMEOUT"
 print
@@ -52,6 +53,8 @@ DURATION=$(( STOP - START ))
 printf "TOOK: %5.2f s\n" $DURATION
 printf "RATE: %5.2f MB/s\n" \
        $(( float(${A[size]}) / DURATION / (1024*1024) ))
+DATE_FMT_NICE=%D{%Y-%m-%d} %D{%H:%M}
+print  "DONE: ${(%)DATE_FMT_NICE}"
 
 # Local Variables:
 # buffer-file-coding-system:utf-8-unix
